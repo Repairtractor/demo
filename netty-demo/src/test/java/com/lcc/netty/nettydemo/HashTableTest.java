8e7603cc41e46610989f75b7ded811e019b59ac7
lcc <liuchuangchuang@yintatech.com>
2023-09-13 15:17:58 +0800

    添加netty模块

---
+++ netty-demo/src/test/java/com/lcc/netty/nettydemo/HashTableTest.java
@@ -0,0 +1,13 @@
+package com.lcc.netty.nettydemo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Hashtable;
+
+public class HashTableTest {
+    @Test
+    void test(){
+        Hashtable<Object, Object> objectObjectHashtable = new Hashtable<>();
+
+    }
+}
