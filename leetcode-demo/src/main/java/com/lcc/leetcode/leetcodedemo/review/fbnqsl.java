24c5755735ec070656aacf0da4e4e376f68a6510
liuchuangchuang <13197149229@163.com>
2025-03-03 17:56:35 +0800

    学习动态规划

---
+++ leetcode-demo/src/main/java/com/lcc/leetcode/leetcodedemo/review/fbnqsl.java
@@ -0,0 +1,54 @@
+package com.lcc.leetcode.leetcodedemo.review;
+
+public class fbnqsl {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        System.out.println(solution1.fib(5));
+    }
+
+
+    class Solution {
+        public int climbStairs(int n) {
+            int[] dp = new int[n + 1];
+            if (n <= 1) {
+                return n;
+            }
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+
+    static class Solution1 {
+        public int fib(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            int[] dp = new int[n+1];
+            dp[1] = 1;
+            dp[2] = 1;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+
+    class Solution2 {
+        ////只需要记录一个历史最小值，然后记录最小值与另一个值的最大差值就行了
+        /// [7,1,5,3,6,4]
+        public int maxProfit(int[] prices) {
+            int min = prices[0],df=0;
+            for (int price : prices) {
+                min = Math.min(min, price);
+                df = Math.max(df, price - min);
+            }
+            return df;
+        }
+    }
+
+
+}
